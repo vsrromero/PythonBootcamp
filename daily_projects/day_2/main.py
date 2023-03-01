f33bb083afbfb285f76afc90e8eeb4dd4b19cc9a
Victor Romero <42779507+vsrromero@users.noreply.github.com>
2023-03-01 00:19:21 +0000

    Delete daily_projects/day_2 directory

--- daily_projects/day_2/main.py
+++
@@ -1,7 +0,0 @@
-total_bill = float(input('What was the total bill?\n'))
-tip_rate = float(input('How much tip would you like to give? 10, 12 or 15?\n'))
-people = int(input('How many people to split the bill?\n'))
-
-tip = round(total_bill * tip_rate / 100, 2)
-bill_per_person = round((total_bill + tip) / people, 2)
-print(bill_per_person)
